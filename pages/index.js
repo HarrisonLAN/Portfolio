aef030e4b7b960ec18e46686e52f82e9e833720f
HarrisonLAN <robharrison0399@gmail.com>
2021-08-22 23:12:32 +0100

    working on index page

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,33 @@
-import Head from 'next/head'
+import Image from 'next/image'
 import Navbar from '../components/nav'
 import Footer from '../components/footer'
+import Head from 'next/head'
+
 
 export default function Home() {
   return (
-    <div class="h-screen bg-primary">
-      <Navbar />
-
-      <footer />
-    </div>
-
-
+    <html>
+      <body>
+        <div className="md:font-body h-screen bg-primary ">
+          <Navbar />
+          <div className="w-9/12 h-screen m-auto">
+            <div className="relative h-5/6">
+              <div className="w-96 z-10 absolute top-0 left-10 pl-24 pt-64 text-7xl">
+                <h1>
+                  I’m Robert Harrison, a software engineer based in UK. Available for freelance & collaborations.
+                </h1>
+              </div>
+              <div className="z-0 absolute top-0 right-0 pr-24 pt-16 " >
+                <Image src="/background.png" alt="me" width="600" height="600" className=" object-contain" />
+              </div>
+            </div>
+            <div className=" border-b-4 border-fuchsia-600">
+              <h2 className="text-5xl"> Projects </h2>
+            </div>
+          </div>
+          <Footer />
+        </div>
+      </body>
+    </html>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,22 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-
     backgroundColor: theme => ({
       ...theme('colors'),
       'primary': '#F5F5F5',
     }),
-    extend: {},
+    extend: {
+      spacing: {
+        '96': '50rem',
+      }
+    },
+    fontFamily: {
+      'display': ['Oswald',],
+      'body': ['"Open Sans"',],
+      'sans': ['ui-sans-serif', 'system-ui',],
+      'serif': ['ui-serif', 'Georgia',],
+      'mono': ['ui-monospace', 'SFMono-Regular',],
+    }
   },
   variants: {
     extend: {},
